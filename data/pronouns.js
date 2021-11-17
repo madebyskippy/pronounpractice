ae3305b313d46595f5dbc0af0dba55d7f0780914
madebyskippy <timxsun@gmail.com>
2021-11-17 15:55:06 -0500

    randomizing pronouns each turn

---
+++ data/pronouns.js
@@ -0,0 +1,12 @@
+var staticPronouns = [
+"he/him",
+"she/her",
+"they/them",
+"she/they",
+"he/they",
+"any pronouns"];
+var transitionPronouns = [
+"he/him → she/her",
+"she/her → he/him",
+"she/her → they/them",
+"he/him → they/them"];
